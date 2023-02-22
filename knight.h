74c22109a578a2473dbed78bbefc3cb1e9fb1ad4
khanhnhanbk <nhanhoa21012002@gmail.com>
2023-02-22 09:52:35 +0700

    testing

--- knight.h
+++ knight.h
@@ -3,29 +3,7 @@
 
 #include "main.h"
 
-// global variables
-
-
 void display(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int rescue);
 void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue);
 
-// helper functions
-int calculateLevel(int index);
-bool is_prime(int n);
-int nearest_prime(int n);
-bool is_knight_dead(int HP,int &phoenixdown, int & rescue, int & state);
-std::string tolower(std::string s);
-int has_merlin(std::string s);
-// handle_event functions
-void handle_event_1_to_5(int index,int event, int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue, int &);
-void handle_event_6(int index,int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue, int & state, int & state_end);
-void handle_event_7(int index,int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue, int & state, int & state_end);
-void handle_event_11(int &HP, int level, int phoenixdown);
-void handle_event_12(int &HP);
-void handle_event_13(int index, int event_code, int &HP, int &level, int &remedy, int &maidenkiss, int &phoenixdown, int &rescue, int &state, int &state_end, std::string filename);
-void handle_event_15(int &HP, int &remedy, int&state);
-void handle_event_16(int &level, int &maidenkiss, int&state);
-void handle_event_18(int & HP, string filename);
-void handle_event_19(int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue, int & state, string filename);
-void handle_event_99(int &level, int &rescue);
 #endif // __KNIGHT_H__
--- main.cpp
+++ main.cpp
@@ -1,11 +1,84 @@
 #include "knight.h"
 
+bool compareFiles(std::string filename1, std::string filename2)
+{
+    std::ifstream file1(filename1);
+    std::ifstream file2(filename2);
+    char char1, char2;
 
-int main(int argc, char ** argv) {
-    string file_input = (argv[1]);
+    if (!file1 || !file2)
+    {
+        return false;
+    }
 
-    int HP, level, remedy, maidenkiss, phoenixdown, rescue;
-    adventureToKoopa(file_input, HP, level, remedy, maidenkiss, phoenixdown, rescue);
-    
+    while (file1.get(char1) && file2.get(char2))
+    {
+        if (char1 != char2)
+        {
+            return false;
+        }
+    }
+
+    return char1 == char2;
+}
+int main()
+{
+    for (int i = 1; i < 3; i++)
+    {
+        string folder = "./testcases/test" + to_string(i) + "/";
+        ofstream out(folder + "output");
+        streambuf *coutbuf = cout.rdbuf();
+        cout.rdbuf(out.rdbuf());
+
+        string file_input = folder + "tc1_input";
+
+        // change line 3 in tc1_input
+        ifstream file(file_input);
+        string line1, line2, line3;
+        getline(file, line1);
+        getline(file, line2);
+        getline(file, line3);
+        // add folder to line
+        string file_mush_ghost = line3.substr(0, line3.find(','));
+        string file_asclepius_pack = line3.substr(line3.find(',') + 1, line3.find_last_of(',') - line3.find(',') - 1);
+        string file_merlin_pack = line3.substr(line3.find_last_of(',') + 1, line3.length() - line3.find_last_of(',') - 1);
+
+        file_mush_ghost = folder + file_mush_ghost;
+        file_asclepius_pack = folder + file_asclepius_pack;
+        file_merlin_pack = folder + file_merlin_pack;
+        string line3_new = file_mush_ghost + "," + file_asclepius_pack + "," + file_merlin_pack;
+        file.close();
+        // rewrite line 3 in tc1_input
+        ofstream file2(file_input);
+        file2 << line1 << endl
+              << line2 << endl
+              << line3_new << endl;
+        file2.close();
+
+        int HP, level, remedy, maidenkiss, phoenixdown, rescue;
+        adventureToKoopa(file_input, HP, level, remedy, maidenkiss, phoenixdown, rescue);
+
+        ofstream file3(file_input);
+        file3 << line1 << endl
+              << line2 << endl
+              << line3 << endl;
+        file3.close();
+
+        // compare output
+        // Restore cout
+        cout.rdbuf(coutbuf);
+        string file_output = folder + "output";
+        string file_output_expected = folder + "expected";
+        
+        bool isSame = compareFiles(file_output, file_output_expected);
+        if (isSame)
+        {
+            cout << "Testcase " << i << " passed" << endl;
+        }
+        else
+        {
+            cout << "Testcase " << i << " failed" << endl;
+        }
+    }
     return 0;
 }
